6d804cb61f1efbd5bf80b386227f5129723e3bf6
swift code solution <dev@swiftcodesolution.com>
2025-12-04 21:17:43 +0500

    002 chat module + profile page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
-  params: Promise<{ deviceId: string }>;
 };
 
 export default async function DashboardLayoutWrapper({ children }: Props) {
